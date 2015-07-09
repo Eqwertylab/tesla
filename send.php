3a58f74d36705b4d9986f1eb774aba3b62a927e6
Eqwertylab <79225555735@ya.ru>
2015-07-09 22:04:27 +0500

    add form sending1

--- send.php
+++ send.php
@@ -25,10 +25,10 @@
 
   function mail_utf8($to, $from_user, $from_email, $subject = '(No subject)', $message = '') 
   { 
-    $from_user = "=?UTF-8?B?".base64_encode($from_name)."?=";
+    $from_user = "=?UTF-8?B?".base64_encode($from_user)."?=";
     $subject = "=?UTF-8?B?".base64_encode($subject)."?=";
 
-    $headers =  "From: $from_name <$target_mail>\r\n". 
+    $headers =  "From: $from_user <$from_email>\r\n". 
                 "MIME-Version: 1.0" . "\r\n" . 
                 "Content-type: text/html; charset=UTF-8" . "\r\n"; 
 
